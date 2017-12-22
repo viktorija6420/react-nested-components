9b64200435dea3e8c35112a66a5bb4e19f9da6a2
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-12-22 09:32:29 -0500

    set body state to new body when button is clicked

--- src/Post.js
+++ src/Post.js
@@ -5,6 +5,20 @@ import Author from './Author.js';
 
 
 class Post extends Component {
+  constructor (props){
+    super()
+    this.state = {
+      body: props.body
+    }
+  }
+
+  changeBody(e){
+    let newBody = prompt("Put some text for the body")
+    this.setState({
+      body: newBody
+    })
+  }
+
   render() {
     let allComments = [
       <Comment body={this.props.comments[0]} />,
@@ -20,12 +34,13 @@ class Post extends Component {
       )
     }/>
 
-
     return (
       <div>
         <h1>{this.props.title}</h1>
         <h4><Author author={allAuthors}/></h4>,
-        {this.props.body}
+        {this.state.body}
+        <div><button onClick={(e) => this.changeBody(e)}>Change Body!</button></div>
+
         <h3>Comments:</h3><Comment body={allComments}/>
       </div>
     );
